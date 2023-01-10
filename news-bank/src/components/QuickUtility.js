619a0261e8caf020baa60d370275cd7563dba76d
Võ Trần Quang Thông <64117392+thongsaint2209@users.noreply.github.com>
2023-01-10 21:28:01 +0700

    quick utility

--- news-bank/src/components/QuickUtility.js
+++ news-bank/src/components/QuickUtility.js
@@ -2,7 +2,7 @@ import { Box, Button, Paper, Typography } from "@mui/material";
 import TransferWithinBank from '../assets/img/Quick utility/TransferWithinBank.svg';
 import InterbankTransfer from '../assets/img/Quick utility/InterbankTransfer.svg';
 import RecentTransaction from '../assets/img/Quick utility/RecentTransaction.svg';
-
+import RightArrow from '../assets/icon/RightArrow.svg';
 
 export default function QuickUtility() {
     return(
@@ -15,21 +15,30 @@ export default function QuickUtility() {
                 <Typography className='text-black text-left' style={{fontWeight:600}}>Quick Utility</Typography>
                 <div className="grid grid-rows-3 gap-y-5">
                         <Box className="flex border-2 rounded-md">
-                            <Button className="flex flex-row flex-grow justify-items-stretch ">
-                                <img src={TransferWithinBank} alt='TransferWithinBank'/>
-                                <Typography className='text-black text-left' style={{textTransform: 'none'}}>Transfer Within Bank</Typography>
+                            <Button className="flex flex-grow">
+                                <Box className="flex flex-row flex-grow justify-between px-2">
+                                    <img src={TransferWithinBank} alt='TransferWithinBank'/>
+                                    <Typography className='text-black text-left self-center' style={{textTransform: 'none'}}>Transfer Within Bank</Typography>
+                                    <img src={RightArrow} alt='RightArrow'/>
+                                </Box>
                             </Button>
                         </Box>
                         <Box className="flex border-2 rounded-md">
-                            <Button className="flex flex-row flex-grow justify-around ">
-                                <img src={InterbankTransfer} alt='InterbankTransfer'/>
-                                <Typography className='text-black text-left' style={{textTransform: 'none'}}>Interbank Transfer</Typography>
+                            <Button className="flex flex-grow">
+                                <Box className="flex flex-row flex-grow justify-between px-2">
+                                    <img src={InterbankTransfer} alt='InterbankTransfer'/>
+                                    <Typography className='text-black text-left self-center' style={{textTransform: 'none'}}>Interbank Transfer</Typography>
+                                    <img src={RightArrow} alt='RightArrow'/>
+                                </Box>
                             </Button>
                         </Box>
                         <Box className="flex border-2 rounded-md">
-                            <Button className="flex flex-row flex-grow justify-around ">
-                                <img src={RecentTransaction} alt='RecentTransaction'/>
-                                <Typography className='text-black text-left' style={{textTransform: 'none'}}>Recent Transactions</Typography>
+                            <Button className="flex flex-grow">
+                                <Box className="flex flex-row flex-grow justify-between px-2">
+                                    <img src={RecentTransaction} alt='RecentTransaction'/>
+                                    <Typography className='text-black text-left self-center' style={{textTransform: 'none'}}>Recent Transactions</Typography>
+                                    <img src={RightArrow} alt='RightArrow'/>
+                                </Box>
                             </Button>
                         </Box>
                 </div>
