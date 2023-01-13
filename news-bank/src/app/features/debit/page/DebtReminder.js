7f58756afb6e09718ec81ddf16b73025474dab6d
tnxp21 <tnxp2001@gmail.com>
2023-01-13 23:51:33 +0700

    Side bar

--- news-bank/src/app/features/debit/page/DebtReminder.js
+++ news-bank/src/app/features/debit/page/DebtReminder.js
@@ -12,20 +12,20 @@ export default function DebtReminder () {
             sx={{ borderRadius: '10px' }}
         >
             <Link to='createDebtReminder' style={{ textDecoration: 'none' }}>
-                <div className="flex rounded-lg" style={{ backgroundColor: '#DAEEFF', height: '10vw', width: '10vw' }}>
+                <div className="flex items-center justify-center rounded-lg py-2" style={{ backgroundColor: '#DAEEFF', height: '10vw', width: '12vw' }}>
                     <Button className="flex flex-grow">
                         <div className="flex flex-grow flex-col items-center gap-y-2">
                             <div style={{ height: '59px', width: '59px' }}><img src={CreateDebt} alt='CreateDebt'/></div>
-                            <Typography className='text-black' style={{ textTransform: 'none' }}>Transfer Within Bank</Typography>
+                            <Typography className='text-black' style={{ textTransform: 'none' }}>Create Debt Reminder</Typography>
                         </div>
                     </Button>
                 </div>
             </Link>
-            <div className="flex rounded-lg" style={{ backgroundColor: '#DAEEFF', height: '10vw', width: '10vw' }}>
+            <div className="flex items-center justify-center rounded-lg py-2" style={{ backgroundColor: '#EBE4F4', height: '10vw', width: '12vw' }}>
                 <Button className="flex flex-grow">
                     <div className="flex flex-grow flex-col items-center gap-y-2">
                         <div style={{ height: '59px', width: '59px' }}><img src={ViewDebt} alt='ViewDebt'/></div>
-                        <Typography className='text-black' style={{ textTransform: 'none' }}>Interbank Transfer</Typography>
+                        <Typography className='text-black' style={{ textTransform: 'none' }}>View Debt List and Debt Payment</Typography>
                     </div>
                 </Button>
             </div>
