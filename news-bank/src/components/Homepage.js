e87fc02dabd2a1d0ad09b6c51eb5daf992b9635a
Võ Trần Quang Thông <64117392+thongsaint2209@users.noreply.github.com>
2023-01-14 00:53:32 +0700

    Side bar nav bar user admin

--- news-bank/src/components/Homepage.js
+++ news-bank/src/components/Homepage.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import Navbar from './NavBar'
-import QuickUtility from './QuickUtility'
-import Sidebar from './SideBar'
+import Navbar from './Navbar/NavBarUser'
+import QuickUtilityUser from './QuickUtility/QuickUtilityUser'
+import QuickUtilityEmployee from './QuickUtility/QuickUtilityEmployee'
+import SideBarUser from './Sidebar/SideBarUser'
+import SideBarAdmin from './Sidebar/SideBarAdmin'
+import SideBarEmployee from  './Sidebar/SideBarEmployee'
 
 export default function Homepage ({ child }) {
-  return (
+    const roleID = parseInt(localStorage.role_id);
+    return (
         <div className='h-screen grid grid-cols-5 gap-x-5' style={{ background: '#F8F9FD' }}>
-            <Sidebar/>
+            {roleID == 1 ? <SideBarAdmin /> : roleID == 2 ? <SideBarUser />: <SideBarEmployee/>}
             <div className='col-span-4 grid grid-rows-6 '>
                 <Navbar/>
                 <div className='row-span-5 grid grid-cols-7 gap-x-5 mx-3'>
@@ -14,10 +18,10 @@ export default function Homepage ({ child }) {
                         {child}
                     </div>
                     <div className='col-span-2'>
-                        <QuickUtility/>
+                    {roleID == 2 ? <QuickUtilityUser/>: roleID == 3 ? <QuickUtilityEmployee/>:<></>} 
                     </div>
                 </div>
             </div>
         </div>
-  )
+    )
 }
