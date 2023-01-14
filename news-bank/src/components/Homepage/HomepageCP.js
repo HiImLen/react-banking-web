9c3877f1e879b9e79211d47bef9ba46959bc37b8
Võ Trần Quang Thông <64117392+thongsaint2209@users.noreply.github.com>
2023-01-14 16:01:45 +0700

    fix nav bar homepage

--- news-bank/src/components/Homepage/HomepageCP.js
+++ news-bank/src/components/Homepage/HomepageCP.js
@@ -34,55 +34,7 @@ export default function HomepageCP() {
     return (
         <>
             {role_id === 2 ? (
-                // Những phần ở giữa hompage và ko bao gồm QuickUtility
-                <div className="grid grid-rows-5 gap-y-14">
-                    {/* Chi tiết thông tin card */}
-                    <Paper
-                        className='row-span-3 grid grid-rows-6 py-3 px-5'
-                        elevation={2}
-                        sx={{ borderRadius: '10px' }}
-                    >
-                        <div className="grid grid-cols-2">
-                            <Typography className='text-black' style={{ fontWeight: 600 }}>My Card</Typography>
-                            <Typography className='text-black text-left' style={{ fontWeight: 600 }}>Details</Typography>
-                        </div>
-                        <div className="row-span-5 grid grid-cols-2">
-                            {/* Thông tin thẻ trên hình */}
-                            <Box
-                                className="grid grid-rows-2 p-4 mr-8"
-                                sx={{
-                                    backgroundImage: `url(${ATMPic})`,
-                                    backgroundRepeat: 'no-repeat',
-                                    backgroundSize: 'contain',
-                                    aspectRatio: '2/1'
-                                }}
-                            >
-                                <div className="invisible"></div>
-                                <div className="grid grid-rows-2">
-                                    <Typography className='text-white' style={{ fontWeight: 600 }}>{accountInfo.number}</Typography>
-                                    <Typography className='text-white' style={{ textTransform: 'uppercase' }}>{name}</Typography>
-                                </div>
-                            </Box>
-                            {/* Thông tin chi tiết */}
-                            <div className="grid grid-rows-2 content-center">
-                                <div className="grid grid-cols-2 content-center py-3">
-                                    <div className="flex flex-col">
-                                        <Typography>Balance</Typography>
-                                        <Typography className='text-black' style={{ fontWeight: 600 }}>{balance} VND</Typography>
-                                    </div>
-                                    <div className="flex flex-col">
-                                        <Typography>Account number</Typography>
-                                        <Typography className='text-black' style={{ fontWeight: 600 }}>{accountInfo.number}</Typography>
-                                    </div>
-                                </div>
-                                <div className="grow flex flex-col border-t-2 py-3">
-                                    <Typography>Branch opened the account</Typography>
-                                    <Typography className='text-black' style={{ fontWeight: 600 }}>SWENBANK SAIGON</Typography>
-                                </div>
-                            </div>
-                        </div>
-                    </Paper>
-                </div>
+                
             ) : <></>}
 
             {/* Các chức năng ở dưới */}
@@ -91,26 +43,7 @@ export default function HomepageCP() {
                 elevation={2}
                 sx={{ borderRadius: '10px' }}
             >
-                <Link to='transfer' style={{ textDecoration: 'none' }}>
-                    <div className="flex rounded-lg" style={{ backgroundColor: '#DAEEFF', height: '10vw', width: '10vw' }}>
-                        <Button className="flex flex-grow">
-                            <div className="flex flex-grow flex-col items-center gap-y-2">
-                                <div style={{ height: '59px', width: '59px' }}><img src={FundTransfer} alt='FundTransfer' /></div>
-                                <Typography className='text-black' style={{ textTransform: 'none' }}>{role_id === 1 ? "Employee Manager" : role_id === 2 ? "Fund Transfer" : role_id === 3 ? "Create Customer Account" : ""}</Typography>
-                            </div>
-                        </Button>
-                    </div>
-                </Link>
-                <Link to='debt' style={{ textDecoration: 'none' }}>
-                    <div className="flex rounded-lg" style={{ backgroundColor: '#DAEEFF', height: '10vw', width: '10vw' }}>
-                        <Button className="flex flex-grow">
-                            <div className="flex flex-grow flex-col items-center gap-y-2">
-                                <div style={{ height: '59px', width: '59px' }}><img src={DebtReminder} alt='DebtReminder' /></div>
-                                <Typography className='text-black' style={{ textTransform: 'none' }}>{role_id === 1 ? "Transactions With Other Banks" : role_id === 2 ? "Debt Reminder Management" : role_id === 3 ? "Deposit Money" : ""}</Typography>
-                            </div>
-                        </Button>
-                    </div>
-                </Link>
+                
                 {role_id === 1 ? (
                     <Link to='/admin' style={{ textDecoration: 'none' }}>
                         <div className="flex rounded-lg" style={{ backgroundColor: '#DAEEFF', height: '10vw', width: '10vw' }}>
